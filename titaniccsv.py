20afd234a29a36e0f16e47033d46c1b07440cebe
ToivoPH <153418959+ToivoPH@users.noreply.github.com>
2024-03-17 09:37:00 +0000

    foundAges

--- titaniccsv.py
+++ titaniccsv.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-print('hello')
+print('running')
 
 df = pd.read_csv("titanic_cleaned.csv")
 
 df.iloc[1:3]
 #iloc is rows then columns (:,:)
 
-df[df['Age'] <= 16]
+#df[df['Age'] <= 16]
 """
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -20,11 +20,12 @@ plt.savefig('demotitanic.png') #save this graphic to your computer
 """
 
 #task 1
+"""
 df[['Sex', 'Fare']].groupby('Sex').sum()
-
+"""
 
 #task 2
-import matplotlib.pyplot as plt
+"""import matplotlib.pyplot as plt
 titanic = pd.read_csv('titanic_cleaned.csv')
 
 bins = [0, 16, 60, df['Age'].max()]
@@ -34,11 +35,15 @@ df['Age_category'] = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
 #print(df['Age_category'])
 category_count = df['Age_category'].value_counts()
 print(category_count)
-plt.pie(category_count, explode=(0, 0.1, 0), labels=category_count.index, autopct='%1.1f%%', startangle=90)
+plt.pie(category_count, explode=(0, 0.1, 0), labels=category_count.index, autopct='%1.2f%%', startangle=90)
 plt.show()
 plt.savefig('demopieagetitanic.png')
+"""
 
-#Using a Pie chart, create 3 categories that shows the survival rates based on age:
-#Children: > 16
-#Adults: 16 < x < 60
-#Elderly: > 61
+#task 3 
+#what are the chances of a child surviving
+
+under_30 = df[df['Age'] < 30]
+
+psurvive =  len(under_30) / len(df)
+print(f"{psurvive * 100}%")
